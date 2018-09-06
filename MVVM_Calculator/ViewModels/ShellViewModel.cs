78b207b6b7fa2657f379c3275ff00883d11bef56
Sebastian Rogacki <S.Rogacki2012@gmail.com>
2018-09-06 16:51:06 +0200

    Tasten und Display Funktion hinzugefügt

--- MVVM_Calculator/ViewModels/ShellViewModel.cs
+++ MVVM_Calculator/ViewModels/ShellViewModel.cs
@@ -4,18 +4,39 @@ using MVVM_Calculator;
 using MVVM_Calculator.ExtensionMethods;
 using System.Data;
 using System.Collections.Generic;
+using Prism.Commands;
+using MVVM_Calculator.ViewModels.Bases;
+using System;
 
 namespace MVVM_Calculator.ViewModels
 {
-    class ShellViewModel : BindableBase
+    class ShellViewModel : ViewModelBase
     {
 
 
         /// <summary>
         /// DataBinding für MainWindow Titel
         /// </summary>
-        public string Title { get; set; } = "MVVM - Calculator";
-        
+        public string Title { get; } = "MVVM - Calculator";
 
+        private string _calculatorText;
+
+        public string CalculatorText
+        {
+            get => _calculatorText;
+            set => SetProperty(ref _calculatorText, value);
+        }
+
+        public DelegateCommand<object> AddNumberCommand { get; set; }
+
+        protected override void RegisterCommands()
+        {
+            AddNumberCommand = new DelegateCommand<object>(AddNumber);
+        }
+
+        private void AddNumber(object buttonValue)
+        {
+            CalculatorText += buttonValue;
+        }
     }
 }
