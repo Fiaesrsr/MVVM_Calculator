eef5062b1a9b545d0f14487c6f08ebbf153c2fb4
Sebastian Rogacki <S.Rogacki2012@gmail.com>
2018-09-06 14:39:34 +0200

    Projektdateien hinzufügen.

---
+++ MVVM_Calculator/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using MVVM_Calculator.App_Start;
+using System.Windows;
+
+namespace MVVM_Calculator
+{
+    /// <summary>
+    /// Interaktionslogik für "App.xaml"
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            new Bootstrapper().Run();
+        }
+    }
+}
---
+++ MVVM_Calculator/ViewModels/ShellViewModel.cs
@@ -0,0 +1,21 @@
+﻿using MVVM_Calculator.Models;
+using Prism.Mvvm;
+using MVVM_Calculator;
+using MVVM_Calculator.ExtensionMethods;
+using System.Data;
+using System.Collections.Generic;
+
+namespace MVVM_Calculator.ViewModels
+{
+    class ShellViewModel : BindableBase
+    {
+
+
+        /// <summary>
+        /// DataBinding für MainWindow Titel
+        /// </summary>
+        public string Title { get; set; } = "MVVM - Calculator";
+        
+
+    }
+}
